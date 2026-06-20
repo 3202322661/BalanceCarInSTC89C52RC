1b67cc5e0b78896411f536f5604c232a92a8c17c
LMY <3202322661@qq.com>
2026-06-20 14:20:59 +0800

    上传Keil项目工程

---
+++ Include/config.h
@@ -0,0 +1,12 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include <reg52.h>
+
+#define FOSC 11059200UL
+
+typedef unsigned char uint8_t;
+typedef unsigned int uint16_t;
+typedef unsigned long uint32_t;
+
+#endif
